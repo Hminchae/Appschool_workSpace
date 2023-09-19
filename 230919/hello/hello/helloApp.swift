bbbaa3553ff8b984a77ad92c09ace0897432b69d
Hminchae <hminchae@gmail.com>
2023-09-19 10:25:24 +0900

    .

---
+++ 230919/hello/hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by 황민채 on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
