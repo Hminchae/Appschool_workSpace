14b526dc8001777dd0be6839a94655fe24ef78cf
Hminchae <hminchae@gmail.com>
2023-09-27 22:26:05 +0900

    practice in playground

---
+++ playground/for_0925.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Foundation
+
+var X:String = "12345"
+var Y:String = "345"
+var arrX = Array(X)
+var arrY = Array(Y)
+print(arrX)
+print(arrY)
+//var newArr =(String)[]
+var arrX= Set<String>()
+let inter = arrX.intersection(arrY)
