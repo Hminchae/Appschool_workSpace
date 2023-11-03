5db9c2561409ab2633a1d67bec77c6c4e08be202
Hminchae <hminchae@gmail.com>
2023-11-03 17:11:04 +0900

    upload  PhotosPickerSample

---
+++ 231103/PhotosPickerSample/PhotosPickerSample/PhotosPickerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerSampleApp.swift
+//  PhotosPickerSample
+//
+//  Created by 황민채 on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
