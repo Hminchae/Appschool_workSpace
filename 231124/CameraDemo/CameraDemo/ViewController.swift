90461adee60abb0ac17f1c5718b9c8a4a0009efc
Hminchae <hminchae@gmail.com>
2023-11-24 14:12:39 +0900

    [Add] CameraDemo

---
+++ 231124/CameraDemo/CameraDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by 황민채 on 11/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func useCamera(_ sender: Any) {
+    }
+    
+    @IBAction func useCameraRoll(_ sender: Any) {
+    }
+}
+
