fe170d9203eb40753d62f08fd6e0b9564a9fe8fe
Hminchae <hminchae@gmail.com>
2023-11-27 20:39:13 +0900

    [ADD] CameraDemo Project

--- 231124/CameraDemo/CameraDemo/ViewController.swift
+++ 231124/CameraDemo/CameraDemo/ViewController.swift
@@ -6,19 +6,74 @@
 //
 
 import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @IBOutlet weak var imageView: UIImageView!
+    var newMedia: Bool? // 뒷 부분에서 필요할 Boolean 속성 선언
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func useCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.mediaTypes = [UTType.image.identifier]
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+            
+            newMedia = true
+        }
     }
     
     @IBAction func useCameraRoll(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.mediaTypes = [UTType.image.identifier]
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+            
+            newMedia = false
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, 
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let mediaType = info[.mediaType] as! NSString
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        if mediaType.isEqual(to: UTType.image.identifier) {
+            let image = info[.originalImage] as! UIImage
+            
+            imageView.image = image
+            
+            if newMedia == true {
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(ViewController.image(image: didFinishSavingWithError: contextInfo:)), nil)
+            }
+        }
+    }
+    @objc func image(image: UIImage,
+                     didFinishSavingWithError error: NSErrorPointer,
+                     contextInfo: UnsafeRawPointer) {
+        if error != nil {
+            print("message: Error(이미지 없음)")
+        }
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
-
