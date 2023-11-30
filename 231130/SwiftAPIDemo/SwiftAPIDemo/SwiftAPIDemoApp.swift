eb9a389c80205172df864037da0f213e0bc237a5
Hminchae <hminchae@gmail.com>
2023-11-30 16:12:38 +0900

    practice NetWork

---
+++ 231130/SwiftAPIDemo/SwiftAPIDemo/SwiftAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAPIDemoApp.swift
+//  SwiftAPIDemo
+//
+//  Created by 황민채 on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
